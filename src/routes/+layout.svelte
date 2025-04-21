bd9e846d8ecb3b8445e2d93757db40c67b3f52fd
Fram <fram.tid.fash@gmail.com>
2025-04-21 08:06:54 -0600

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,57 @@
+<script>
+  import { base } from '$app/paths';
+</script>
+<header>
+  <img src="" alt="logo">
+  <div class="h-center">
+    <h2>Argos Statistics</h2>
+    <div class="links">
+      <a href={`${base}/home`}>home</a>
+      <a href={`${base}/bio`}>bio</a>
+      <a href={`${base}/showcase`}>showcase</a>
+      <a href={`${base}/contact`}>contact</a>
+    </div>
+  </div>
+  <img src="" alt="logo">
+</header>
+<slot/>
+<style>
+  :global(body) {
+    margin: 0;
+    padding: 0;
+    background: #1A1A1A;
+    color: white;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+  }
+
+  header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    padding: 0.5rem 1rem;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    background-image: linear-gradient( 135deg, #F05F57 10%, #360940 100%);
+  }
+
+  .h-center {
+    width: 40%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .links {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    & a {
+      text-decoration: none;
+      color: white;
+    }
+  }
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,114 @@
+<div class="main-container">
+  <h1>Adolfo Almazan Becerril</h1>
+  <h4>Ciencia de Datos & Arquitectura de Sistemas — Optimización de Procesos — Soluciones de
+Software a Medida</h4>
+  <div class="leader-card">
+    <h3>
+      Resumen de Experiencia:
+    </h3>
+    <p>
+      Ayudo a empresas a automatizar flujos de trabajo, diseñar sistemas de datos escalables y desarrollar
+      software personalizado para mejorar eficiencia y toma de decisiones. Mis habilidades integran ciencia de datos,
+      ingenierı́a de software y diseño de procesos, garantizando soluciones técnicas y prácticas.
+    </p>
+    <h3>
+      ¿Por Qué Trabajar Conmigo?
+    </h3>
+    <ul>
+      <li>
+        Enfoque Integral: Combino profundidad técnica con entendimiento del negocio.
+      </li>
+      <li>
+        Orientado a Eficiencia: Soluciones que reducen trabajo manual y escalan.
+      </li>
+      <li>
+        Independiente & Ágil: Sin sobrecostos; proyectos entregados con Retorno de la inversión claro.
+      </li>
+    </ul>
+  </div>
+  <div class="services">
+    <h2>Servicios Principales</h2>
+    <div class="service-list">
+      <ul>
+        <h4>
+          Arquitectura de Datos e Ingeniería
+        </h4>
+        <li>Diseño de pipelines, almacenes de datos y marcos de gobernanza.</li>
+        <li>Migración de sistemas legacy a plataformas modernas (ERL, nube, analisis en tiempo real)</li>
+        <h4>
+          Desarrollo de Software a Medida
+        </h4>
+        <li>Herramientas para automatización, reporting o necesidades específicas.</li>
+        <li>Soluciones full-stack (APIs (servidores web), dashboards (herramientas de administrador), integraciónes)</li>
+        <h4>
+          Optimización de Procesos
+        </h4>
+        <li>Rediseño de flujos de trabajo para eliminar cuellos de botella.</li>
+        <li>Implementación de KPIs (indicadores clave de desempeño) para medir mejoras.</li>
+      </ul>
+    </div>
+    <div class="service-list">
+      <ul>
+        <h4>
+          Ciencia de Datos & Análisis
+        </h4>
+        <li>Modelos predictivos, análisis estadístico y soluciones de IA/ML.</li>
+        <li>"Traducción" de datos para audiencias no técnicas.</li>        
+        <h4>
+          Arquitectura de Sistemas
+        </h4>
+        <li>Diseño de infraestructuras IT seguras y escalables.</li>
+        <li>Implementaciones en la nube/híbridas con equilibrio costo-rendimiento.</li>      
+      </ul>
+    </div>
+    <h1>webos</h1>
+  </div>
+</div>
+<style>
+  .main-container {
+    margin-top: 8rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .leader-card {
+    width: 85%;
+    height: 20rem;
+    padding: 1rem;
+    border-radius: 5px;
+    background-image: linear-gradient( 135deg, #F05F57 10%, #360940 100%);
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    box-shadow: 0 0 10px 2px #F05F57;
+  }
+
+  .services {
+    margin-top: 1rem;
+    width: 85%;
+    height: 30rem;
+    padding: 1rem;
+    display: grid;
+    grid-template-columns: 50% 50%;
+    grid-template-rows: 10% 90%;
+    gap: 2rem;
+
+    & h2 {
+      margin: auto;
+      grid-column: 1/3;
+    }
+  }
+
+  .service-list {
+    height: 100%;
+    width: 100%;
+    border-radius: 5px;
+    box-shadow: 0 0 10px 2px white;
+
+    & h4 {
+      margin-top: 20px;
+    }
+
+  }
+</style>
