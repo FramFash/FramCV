e14b96e955d440cc05587b8fa24557b8c49aa990
Fram <fram.tid.fash@gmail.com>
2025-05-07 13:33:06 -0600

    marker icon

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -15,6 +15,13 @@
   }
 
   onMount(() => {
+    delete (L.Icon.Default.prototype as any)._getIconUrl;
+      L.Icon.Default.mergeOptions({
+        iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+        iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+      });
+
     // Initialize map only in browser
     if (typeof window !== 'undefined') {
       const numericLat = parseCoordinate(lat);
